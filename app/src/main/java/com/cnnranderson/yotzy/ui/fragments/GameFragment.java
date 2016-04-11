5cc276d11fa72b38b9adbd26dd54d51e9bb54b56
Conner <cnnranderson@gmail.com>
2016-04-11 11:41:08 -0700

    General File cleanup and comments applied

--- app/src/main/java/com/cnnranderson/yotzy/ui/fragments/GameFragment.java
+++ app/src/main/java/com/cnnranderson/yotzy/ui/fragments/GameFragment.java
@@ -31,14 +31,6 @@ import rx.android.schedulers.AndroidSchedulers;
 public class GameFragment extends BaseFragment {
 
     // Binded Views
-    @Bind({
-            R.id.die1,
-            R.id.die2,
-            R.id.die3,
-            R.id.die4,
-            R.id.die5
-    })
-    List<ImageButton> diceButtons;
     @Bind(R.id.roll_dice)
     Button rollDiceButton;
     @Bind(R.id.score)
@@ -47,44 +39,37 @@ public class GameFragment extends BaseFragment {
     TextView highscore;
     @Bind(R.id.lowscore)
     TextView lowscore;
+    @Bind({ R.id.die1, R.id.die2, R.id.die3, R.id.die4, R.id.die5 })
+    List<ImageButton> diceButtons;
     @Bind({
-            R.id.one_score,
-            R.id.two_score,
-            R.id.three_score,
-            R.id.four_score,
-            R.id.five_score,
-            R.id.six_score,
-            R.id.threekind_score,
-            R.id.fourkind_score,
-            R.id.fullhouse_score,
-            R.id.smstr_score,
-            R.id.lgstr_score,
-            R.id.chance_score,
-            R.id.yotzy_score
+            R.id.one_score, R.id.two_score, R.id.three_score,
+            R.id.four_score, R.id.five_score, R.id.six_score,
+            R.id.threekind_score, R.id.fourkind_score,
+            R.id.fullhouse_score, R.id.smstr_score, R.id.lgstr_score,
+            R.id.chance_score, R.id.yotzy_score
     })
     List<Button> gridScoreButtons;
 
     // Persisted game vars
-    @State
-    boolean[] diceHeld;
-    @State
-    int[] diceNums;
-    @State
-    int rollsLeft = 3;
-    @State
-    int score = 0;
-    @State
-    HashMap<String, Integer> combos;
-    @State
-    boolean newGameStart = true;
+    @State private boolean newGameStart = true;
+    @State private boolean[] diceHeld;
+    @State private int[] diceNums;
+    @State private int rollsLeft = 3;
+    @State private int score = 0;
+    @State private HashMap<String, Integer> combos;
 
     // Misc. Vars
-    Random rand;
+    private Random rand;
 
+    /**
+     * Game Fragment Constructor -- Builds the initial variables for the game
+     */
     public GameFragment() {
-        diceHeld = new boolean[]{false, false, false, false, false};
-        diceNums = new int[]{0, 0, 0, 0, 0};
+        diceHeld = new boolean[]{ false, false, false, false, false };
+        diceNums = new int[]{ 0, 0, 0, 0, 0 };
         rand = new Random();
+
+        // Init Combo score map
         combos = new HashMap<>();
         combos.put("one_score", -1);
         combos.put("two_score", -1);
@@ -104,6 +89,8 @@ public class GameFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // Setup game
         initPreviousScores();
         initDice();
         initGameBoard();
@@ -117,12 +104,16 @@ public class GameFragment extends BaseFragment {
     @OnClick(R.id.roll_dice)
     public void rollDice() {
         if (rollsLeft > 0) {
-            newGameStart = false;
             rollDiceButton.setEnabled(false);
+            newGameStart = false;
+
+            // Reset dice for new round
             if (rollsLeft == 3) {
                 resetDice();
             }
             rollsLeft--;
+
+            // Roll the dice!
             Observable.from(diceButtons)
                     .doOnNext(this::showHideDice)
                     .finallyDo(() -> {
@@ -135,7 +126,9 @@ public class GameFragment extends BaseFragment {
                     .delay(500, TimeUnit.MILLISECONDS)
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe();
+
         } else if (rollsLeft == 0) {
+            // Force player to make a move before continuing to next round
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.StyledDialog);
 
             builder.setMessage("You must choose a combo before rolling again!")
@@ -145,15 +138,19 @@ public class GameFragment extends BaseFragment {
             AlertDialog dialog = builder.create();
             dialog.show();
         } else {
+            // Game over -- Ask to continue playing
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.StyledDialog);
 
             builder.setMessage("Would you like to play again?")
                     .setTitle("Game Over")
                     .setNegativeButton("No", (dialog1, which) -> dialog1.dismiss())
                     .setPositiveButton("Yes", (dialog2, which) -> {
+                        // Refresh game board
                         resetDice();
                         resetBoard();
                         initGameBoard();
+
+                        // Setup roll button again
                         rollsLeft = 3;
                         newGameStart = true;
                         rollDiceButton.setText("Start Game! (3)");
@@ -166,9 +163,12 @@ public class GameFragment extends BaseFragment {
 
     @OnClick({R.id.die1, R.id.die2, R.id.die3, R.id.die4, R.id.die5})
     public void dieSelected(ImageButton die) {
+        // Lock a die in place to hold over to next roll
         if (rollsLeft != 3) {
+            // TODO: Change from using content description to data tag
             CharSequence desc = die.getContentDescription();
             int diePos = Character.getNumericValue(desc.charAt(1));
+
             if (desc.charAt(0) == '0') {
                 die.setColorFilter(ContextCompat.getColor(getActivity(), R.color.colorPrimaryLight), PorterDuff.Mode.MULTIPLY);
                 die.setContentDescription("1" + desc.charAt(1));
@@ -181,6 +181,13 @@ public class GameFragment extends BaseFragment {
         }
     }
 
+    /**
+     * Select a combo to place current dice score into.
+     * Dialog prompt will show to help confirm choice, along
+     * with displaying the number of points their choice will allot
+     * them.
+     * @param combo Name of combo that was selected
+     */
     @OnClick({R.id.one_score, R.id.two_score, R.id.three_score, R.id.four_score, R.id.five_score,
             R.id.six_score, R.id.threekind_score, R.id.fourkind_score, R.id.fullhouse_score,
             R.id.smstr_score, R.id.lgstr_score, R.id.chance_score, R.id.yotzy_score})
@@ -213,14 +220,18 @@ public class GameFragment extends BaseFragment {
         }
     }
 
+    /**
+     * Collect lowest and highest score and display them
+     */
     private void initPreviousScores() {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String saveScore = String.valueOf(score);
         if (sp.contains("low")) {
             int val = sp.getInt("low", -1);
             if (val == -1) {
                 lowscore.setText("");
             } else {
-                lowscore.setText("" + val);
+                lowscore.setText(saveScore);
             }
         }
 
@@ -229,19 +240,24 @@ public class GameFragment extends BaseFragment {
             if (val == -1) {
                 highscore.setText("");
             } else {
-                highscore.setText("" + val);
+                highscore.setText(saveScore);
             }
         }
     }
 
+    /**
+     * Initialize the dice and the roll button according to the game state
+     */
     private void initDice() {
+        String rollButtonText;
         if ((rollsLeft == 0 || rollsLeft == 3) && !newGameStart) {
-            rollDiceButton.setText("Next Round! (3)");
+            rollButtonText = "Next Round! (3)";
         } else if (newGameStart) {
-            rollDiceButton.setText("Start Game! (3)");
+            rollButtonText = "Start Game! (3)";
         } else {
-            rollDiceButton.setText("Roll Dice! (" + rollsLeft + ")");
+            rollButtonText = "Roll Dice! (" + rollsLeft + ")";
         }
+        rollDiceButton.setText(rollButtonText);
 
         for (int i = 0; i < 5; i++) {
             ImageButton die = diceButtons.get(i);
@@ -255,22 +271,31 @@ public class GameFragment extends BaseFragment {
         }
     }
 
+    /**
+     * Initialize the combo game board
+     */
     private void initGameBoard() {
         for (String combo : combos.keySet()) {
             setScore(combo, combos.get(combo));
         }
-        scoreTextView.setText("" + score);
+        String saveScore = String.valueOf(score);
+        scoreTextView.setText(saveScore);
     }
 
+    /**
+     * Animate the dice when a roll is made. Dice
+     * that are "held" will not be animated.
+     * @param die ImageButton die to be animated
+     */
     private void showHideDice(ImageButton die) {
         CharSequence desc = die.getContentDescription();
         if (desc.charAt(0) == '0') {
-            // get the final radius for the clipping circle
+            // Get the final radius for the clipping circle
             int finalRadius = Math.max(die.getWidth(), die.getHeight()) / 2;
             int cx = die.getMeasuredWidth() / 2;
             int cy = die.getMeasuredHeight() / 2;
 
-            // create the animator for this view (the start radius is zero)
+            // Create the animator for this view (the start radius is zero)
             Animator anim;
             if (die.getVisibility() == View.INVISIBLE) {
                 int newNum = rand.nextInt(600) % 6;
@@ -305,25 +330,27 @@ public class GameFragment extends BaseFragment {
         }
     }
 
+    /**
+     * Gets the respective dice image depending on the number given.
+     * @param num Id of dice to retrieve
+     * @return Drawable integer id
+     */
     private int getDiceImage(int num) {
         switch (num) {
-            case 0:
-                return R.drawable.ic_dice_1_white_48dp;
-            case 1:
-                return R.drawable.ic_dice_2_white_48dp;
-            case 2:
-                return R.drawable.ic_dice_3_white_48dp;
-            case 3:
-                return R.drawable.ic_dice_4_white_48dp;
-            case 4:
-                return R.drawable.ic_dice_5_white_48dp;
-            case 5:
-                return R.drawable.ic_dice_6_white_48dp;
+            case 0: return R.drawable.ic_dice_1_white_48dp;
+            case 1: return R.drawable.ic_dice_2_white_48dp;
+            case 2: return R.drawable.ic_dice_3_white_48dp;
+            case 3: return R.drawable.ic_dice_4_white_48dp;
+            case 4: return R.drawable.ic_dice_5_white_48dp;
+            case 5: return R.drawable.ic_dice_6_white_48dp;
             default:
                 return R.drawable.ic_dice_1_white_48dp;
         }
     }
 
+    /**
+     * Resets all the dice to non-held position
+     */
     private void resetDice() {
         for (int i = 0; i < diceHeld.length; i++) {
             diceHeld[i] = false;
@@ -334,7 +361,11 @@ public class GameFragment extends BaseFragment {
         }
     }
 
+    /**
+     * Resets all the combo scores and current points
+     */
     private void resetBoard() {
+        // Reset combos
         combos.put("one_score", -1);
         combos.put("two_score", -1);
         combos.put("three_score", -1);
@@ -348,21 +379,35 @@ public class GameFragment extends BaseFragment {
         combos.put("lgstr_score", -1);
         combos.put("chance_score", -1);
         combos.put("yotzy_score", -1);
+
+        // Reset score
         score = 0;
         scoreTextView.setText("0");
     }
 
+    /**
+     * Sets the combo button to the evaluated number from choosing
+     * it, based on the current dice rolled.
+     * @param comboName Name of selected combo
+     * @param value Value to be placed in combo box
+     */
     private void setScore(String comboName, int value) {
         Button tv = getComboBox(comboName);
         if (tv != null) {
             if (value == -1) {
                 tv.setText("");
             } else {
-                tv.setText("" + value);
+                String stringValue = String.valueOf(value);
+                tv.setText(stringValue);
             }
         }
     }
 
+    /**
+     * Retrieves a button with the given combo name
+     * @param comboName Name of combo button to be retrieved
+     * @return Combo Button
+     */
     private Button getComboBox(String comboName) {
         for (Button tv : gridScoreButtons) {
             String IdAsString = tv.getTag().toString();
@@ -373,55 +418,71 @@ public class GameFragment extends BaseFragment {
         return null;
     }
 
+    /**
+     * Saves score upon winning the game to sharedpreferences
+     */
     private void saveScore() {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String saveScore = String.valueOf(score);
         if (sp.contains("low")) {
             if (sp.getInt("low", 99999) > score) {
                 sp.edit().putInt("low", score).apply();
-                lowscore.setText("" + score);
+                lowscore.setText(saveScore);
             }
         } else {
             sp.edit().putInt("low", score).apply();
-            lowscore.setText("" + score);
+            lowscore.setText(saveScore);
         }
 
         if (sp.contains("high")) {
             if (sp.getInt("high", 0) < score) {
                 sp.edit().putInt("high", score).apply();
-                highscore.setText("" + score);
+                highscore.setText(saveScore);
             }
         } else {
             sp.edit().putInt("high", score).apply();
-            highscore.setText("" + score);
+            highscore.setText(saveScore);
         }
     }
 
+    /**
+     * Check for a full game board
+     * @return true if board is full, false otherwise
+     */
+    private boolean checkWin() {
+        for (int val : combos.values()) {
+            if (val == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /////////////////////////////////////////
+    ////                                 ////
+    ////     Dice Evaluation Methods     ////
+    ////                                 ////
+    /////////////////////////////////////////
+    /**
+     * Fetches the value achieved from choosing a certain combo
+     * @param combo Combo chosen
+     * @return Value of dice from choosing the combo
+     */
     private int fetchDiceValueForCombo(String combo) {
         switch (combo) {
-            case "one_score":
-                return checkSingle(1);
-            case "two_score":
-                return checkSingle(2);
-            case "three_score":
-                return checkSingle(3);
-            case "four_score":
-                return checkSingle(4);
-            case "five_score":
-                return checkSingle(5);
-            case "six_score":
-                return checkSingle(6);
-            case "threekind_score":
-                return checkMulti(3);
-            case "fourkind_score":
-                return checkMulti(4);
-            case "fullhouse_score":
-                return checkFullHouse();
-            case "smstr_score":
-                return checkStraight(0);
-            case "lgstr_score":
-                return checkStraight(1);
-            case "chance_score":
-                return diceSum();
+            case "one_score": return checkSingle(1);
+            case "two_score": return checkSingle(2);
+            case "three_score": return checkSingle(3);
+            case "four_score": return checkSingle(4);
+            case "five_score": return checkSingle(5);
+            case "six_score": return checkSingle(6);
+            case "threekind_score": return checkMulti(3);
+            case "fourkind_score": return checkMulti(4);
+            case "fullhouse_score": return checkFullHouse();
+            case "smstr_score": return checkStraight(0);
+            case "lgstr_score": return checkStraight(1);
+            case "chance_score": return diceSum();
             case "yotzy_score":
                 if (checkYotzy()) {
                     return 50;
@@ -433,15 +494,10 @@ public class GameFragment extends BaseFragment {
         }
     }
 
-    private boolean checkWin() {
-        for (int val : combos.values()) {
-            if (val == -1) {
-                return false;
-            }
-        }
-        return true;
-    }
-
+    /**
+     * Check if player has rolled a Yotzy
+     * @return true if dice are in yotzy form, false otherwise
+     */
     private boolean checkYotzy() {
         int num = diceNums[0];
         for (int i : diceNums) {
@@ -452,6 +508,11 @@ public class GameFragment extends BaseFragment {
         return true;
     }
 
+    /**
+     * Gets the value of the dice for a given dice number
+     * @param id Number to sum up
+     * @return sum of all the id's in the current dice roll
+     */
     private int checkSingle(int id) {
         int sum = 0;
         for (int i : diceNums) {
@@ -462,6 +523,11 @@ public class GameFragment extends BaseFragment {
         return sum;
     }
 
+    /**
+     * Gets the value of n-of-a-kind rolls
+     * @param id Amount of dice needed to be matched
+     * @return Sum of all dice, if n-of-a-kind was found
+     */
     private int checkMulti(int id) {
         for (int i = 1; i < 7; i++) {
             if (getDiceCount(i) >= id) {
@@ -471,6 +537,15 @@ public class GameFragment extends BaseFragment {
         return 0;
     }
 
+    /**
+     * Search for consecutive numbers ('Straights'):
+     * Small Straight - 1-4, 2-5, 3-6
+     * Large Straight - 1-5, 2-6
+     * @param id
+     *      Id of straight to search for:
+     *      0 for small straight, 1 for large straight search
+     * @return 30 for small straight, 40 for large straight, and 0 otherwise
+     */
     private int checkStraight(int id) {
         switch (id) {
             case 0:
@@ -493,6 +568,12 @@ public class GameFragment extends BaseFragment {
         }
     }
 
+    /**
+     * Check if the dice contain a certain value
+     * TODO: Very ineffective, as far as complexity is concerned
+     * @param key Dice number to look for
+     * @return true if key is found, false otherwise
+     */
     private boolean contains(int key) {
         for (int i : diceNums) {
             if (i == key) {
@@ -502,6 +583,11 @@ public class GameFragment extends BaseFragment {
         return false;
     }
 
+    /**
+     * Get the count of die with the given id
+     * @param id number to look for
+     * @return number of instances that id appeared in the current roll of dice
+     */
     private int getDiceCount(int id) {
         int count = 0;
         for (int i : diceNums) {
@@ -512,6 +598,10 @@ public class GameFragment extends BaseFragment {
         return count;
     }
 
+    /**
+     * Sums all the dice currently rolled
+     * @return sum of all dice
+     */
     private int diceSum() {
         int sum = 0;
         for (int i : diceNums) {
@@ -520,17 +610,25 @@ public class GameFragment extends BaseFragment {
         return sum;
     }
 
+    /**
+     * Searches for 3-2 rolls, where there are 3 of one number, and 2 of another
+     * @return 25 if full house is found, 0 otherwise
+     */
     private int checkFullHouse() {
         int id_two = -1;
         int id_three = -1;
         for (int i : diceNums) {
-            if (getDiceCount(i) == 2) {
+            // Search for exactly two dice with the same number
+            if (id_two == -1 && getDiceCount(i) == 2) {
                 id_two = i;
             }
-            if (getDiceCount(i) == 3) {
+            // Search for exactly three dice with the same number
+            if (id_three == -1 && getDiceCount(i) == 3) {
                 id_three = i;
             }
         }
+
+        // If two dice types were found, we have a full house
         if (id_two != -1 && id_three != -1) {
             return 25;
         } else {
